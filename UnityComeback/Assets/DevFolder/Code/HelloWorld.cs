cc255e36c7d558ea9be536ec7f7d4988f4f7db4d
AltoTheDev <Baldrianar@gmail.com>
2024-03-23 19:37:33 +0100

    Hello world script and TMP assets

---
+++ UnityComeback/Assets/DevFolder/Code/HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelloWorld : MonoBehaviour
+{
+
+    public Text _text;
+
+    public void ButtonFuction()
+    {
+        _text.text = "Hello World";
+    }
+
+
+    
+}
